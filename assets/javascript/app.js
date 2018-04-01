668cfc5750d039f37faa91cb748b5d83c694d6cc
Adam <spiderwebb2290@gmail.com>
2018-04-01 14:05:06 -0600

    map update2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,95 +1,61 @@
 var map;
-var infowindow;
-var centralLoc = new google.maps.LatLng(39.6322683, -104.9800738);
-var barRequest = {
-    location: centralLoc,
-    radius: 3219,
-    types: ['bar'],
-    openNow: true,
-};
-var cafeRequest = {
-    location: centralLoc,
-    radius: 3219,
-    types: ['cafe'],
-    openNow: true,
-};
-var restaurantRequest = {
-    location: centralLoc,
-    radius: 3219,
-    types: ['restaurant'],
-    openNow: true,
-};
-var movieRequest = {
-    location: centralLoc,
-    radius: 3219,
-    types: ['movie_theater'],
-    openNow: true,
-};
-var clubRequest = {
-    location: centralLoc,
-    radius: 3219,
-    types: ['night_club'],
-    openNow: true,
-};
-var parkRequest = {
-    location: centralLoc,
-    radius: 3219,
-    types: ['park'],
-    openNow: true,
-};
-function initialize() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        center: centralLoc,
-        zoom: 15
-    });
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(barRequest, callback);
-    service.nearbySearch(cafeRequest, callback);
-    service.nearbySearch(restaurantRequest, callback);
-    service.nearbySearch(movieRequest, callback);
-    service.nearbySearch(clubRequest, callback);
-    service.nearbySearch(parkRequest, callback);
-}
-var option = function(type, vicinity, name) {
-    this.type = type,
-    this.vicinity = vicinity,
-    this.name = name
-}
-var establishments = [{}];
-function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-        
-        var optionsArray = [results];
-        optionsArray.forEach(function(item, i) {
-            for(i in item) {
-                option=[item[i].name, item[i].vicinity, item[i].types];
-               
-                // console.log(option.bar);
-                establishments.push(option);
-                // console.log(this.types = [item[i].types]);
-                // console.log(this.vicinity = [item[i].vicinity]);
-            }
-        });
-        // console.log(establishments);
-    }
-}
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent("<div><strong>" + place.name + "</strong></div><div>" + place.vicinity + "</div>");
-        infowindow.open(map, this); 
-    });
-    // var suggestions = (place.name + "address: " + place.vicinity);
-    // console.log(suggestions);
-}
-google.maps.event.addDomListener(window, 'load', initialize)
 
+function initMap() {
+  // Create the map.
+  var pyrmont = {lat: -33.866, lng: 151.196};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 17
+  });
+
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function() {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
+  };
+
+  // Perform a nearby search.
+  service.nearbySearch(
+      {location: pyrmont, radius: 500, type: ['store']},
+      function(results, status, pagination) {
+        if (status !== 'OK') return;
+
+        createMarkers(results);
+        moreButton.disabled = !pagination.hasNextPage;
+        getNextPage = pagination.hasNextPage && function() {
+          pagination.nextPage();
+        };
+      });
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+
+    var li = document.createElement('li');
+    li.textContent = place.name;
+    placesList.appendChild(li);
+
+    bounds.extend(place.geometry.location);
+  }
+  map.fitBounds(bounds);
+}
